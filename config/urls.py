228be6ba6f479764efec794239a80f307e7d94be
byeongyeongtae <bhj691226@naver.com>
2025-06-02 14:56:52 +0900

    눈이 안보여요

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/speech/', include('speech.urls')),
-    path('api/questions/', include('generate-generate_questions.urls')),
+    path('api/questions/', include('generate_questions.urls')),
     path('api/tracking/', include('tracking.urls')),
 ]
